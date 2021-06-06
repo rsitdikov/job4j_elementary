d6e0d587db914def6f724e91af762750feb764a8
rsitdikov <rsitdikov.usp@gmail.com>
2021-06-06 13:14:35 +0500

    6.1. Классический поиск перебором. [#33489 #29853]

--- src/ru/job4j/array/AlgoArray.java
+++ src/ru/job4j/array/AlgoArray.java
@@ -3,12 +3,10 @@ package ru.job4j.array;
 public class AlgoArray {
     public static void main(String[] args) {
         int[] array = new int[] {5, 3, 2, 1, 4};
-        int temp = array[0]; /* переменная для временного хранения значение ячейки с индексом 0. */
-        array[0] = array[3]; /* записываем в ячейку с индексом 0 значение ячейки с индексом 3. */
-        array[3] = temp; /* записываем в ячейку с индексом 3 значение временной переменной. */
-        for (int i = 0; i < array.length - 2; i++) {
+        int temp;
+        for (int i = 0; i < array.length; i++) {
             temp = array[i];
-            for (int j = i + 1; j < array.length - 1; j++) {
+            for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < temp) {
                     temp = array[j];
                     array[j] = array[i];

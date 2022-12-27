b0f18c4a6c009d1f13303d88e86a325626f2dfb3
rsitdikov <rsitdikov.usp@gmail.com>
2022-12-27 11:47:19 +0500

    6.0. Абстрактные классы [#504949]

---
+++ src/ru/job4j/oop/Vehicle.java
@@ -0,0 +1,57 @@
+package ru.job4j.oop;
+
+public abstract class Vehicle {
+    private String brand;
+    private String model;
+    private String type;
+    private int passengers;
+    private int maxSpeed;
+
+    public abstract void accelerate();
+
+    public abstract void steer();
+
+    public void brake() {
+        System.out.println("Стандартная тормозная система");
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(int passengers) {
+        this.passengers = passengers;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+}
